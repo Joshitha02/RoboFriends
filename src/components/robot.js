8d88de7b0d60df2b9ebf3b9a65638174c4ec2101
K Joshitha Reddy <107829704+Joshitha02@users.noreply.github.com>
2023-07-02 14:48:17 +0530

    used reactt

---
+++ src/components/robot.js
@@ -0,0 +1,27 @@
+export const robot=[
+    {
+        id: 1 ,
+        name:"lala",
+        username:'lalaB',
+        email:"happy@gmail.come"
+    },
+    {
+        id: 2 ,
+        name:"oooo",
+        username:'oooB',
+        email:"haha@gmail.come"
+    },
+    {
+        id: 3 ,
+        name:"papa",
+        username:'papaB',
+        email:"happiee@gmail.come"
+    },
+    {
+        id: 4 ,
+        name:"bbbb",
+        username:'tatat',
+        email:"tatat@gmail.come"
+    },
+
+]
